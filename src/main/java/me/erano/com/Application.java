efc1f701e713dbec13af993ac27fd9890e24c998
rokel <muhammedalisert01@gmail.com>
2025-04-02 01:17:09 +0300

    Application Hexagon(Core Hexagon),Framework Hexagon, Domain Hexagon general project structure created push.

--- src/main/java/me/erano/com/Application.java
+++ src/main/java/me/erano/com/Application.java
@@ -1,4 +1,12 @@
 package me.erano.com;
 
+import me.erano.com.framework.adapters.RouterViewCLIAdapter;
+
 public class Application {
+
+    public static void main(String... args) {
+        var cli = new RouterViewCLIAdapter();
+        var type = "CORE";
+        System.out.println(cli.obtainRelatedRouters(type));
+    }
 }
